59a13c95f8b6a863e469ff788724bc623362a38a
Pdrewicz <106173218+Pdrewicz@users.noreply.github.com>
2022-05-28 02:18:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function clock()
     var day = new Date();
     
     var hour = day.getHours();
-    if (hour<10) hour = "0"+day;
+    if (hour<10) hour = "0"+hour;
     var min = day.getMinutes();
     if (min<10) min = "0"+min;
     var sec = day.getSeconds();
